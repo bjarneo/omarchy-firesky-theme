43fa153bc23a90372d84e5aec28d6740fd85771f
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2025-09-11 19:42:22 +0200

    Use stronger contrast

--- neovim.lua
+++ neovim.lua
@@ -1,12 +1,12 @@
 return {
-	{
-		"bjarneo/firesky.nvim",
-		name = "firesky",
-	},
 	-- {
-	-- 	dir = "/home/bjarneo/Code/firesky.nvim",
+	-- 	"bjarneo/firesky.nvim",
 	-- 	name = "firesky",
 	-- },
+	{
+		dir = "/home/bjarneo/Code/firesky.nvim",
+		name = "firesky",
+	},
 	{
 		"LazyVim/LazyVim",
 		opts = {
